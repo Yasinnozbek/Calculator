ecb3445f641cb8ab985925ebde9ffcbc24c260ff
Yasin Ozbek <yasin09.yz@gmail.com>
2022-04-10 22:12:02 +0300

    Bölme işlemi eklendi,iyileştirmeler yapıldı.

--- Calculator.java
+++ Calculator.java
@@ -3,9 +3,39 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Toplamak istediğiniz iki sayıyı giriniz.");
-        double sayi1 = scanner.nextDouble();
-        double sayi2 = scanner.nextDouble();
-        System.out.println("Toplam : " + (sayi1 + sayi2));
+
+
+        while (true) {
+            System.out.println("Yapmak istediğiniz işlemi seçiniz:\n" +
+                    "1:Toplama\n" + "2:Bölme\n" + "3:Çıkış");
+            String secim = scanner.nextLine();
+
+
+            if (secim.equals("1")) {
+                System.out.println("Toplamak istediğiniz iki sayıyı giriniz.");
+                double sayi1 = scanner.nextDouble();
+                double sayi2 = scanner.nextDouble();
+                scanner.nextLine();
+                System.out.println("Sonuç : " + (sayi1 + sayi2) + "\n**********************************");
+
+
+            }
+
+            else if (secim.equals("2")) {
+                System.out.println("Bölmek istediğiniz iki sayıyı giriniz.");
+                double sayi1 = scanner.nextDouble();
+                double sayi2 = scanner.nextDouble();
+                scanner.nextLine();
+                System.out.println("Sonuç : " + (sayi1 / sayi2) + "\n**********************************");
+            }
+
+            else if (secim.equals("3")) {
+                System.out.println("Çıkış yapılıyor...");
+                break;
+
+            } else {
+                System.out.println("**********************************\n" + "Lütfen geçerli bir işlem seçiniz.");
+            }
+        }
     }
 }
